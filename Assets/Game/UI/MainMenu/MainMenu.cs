500bcbe8c4f15f8349dfc0cccee6eff5306b15a3
Nestor Rubio <nestor.kxs@gmail.com>
2018-03-02 17:03:57 +0100

    Working on UI

---
+++ Assets/Game/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+	private string scenesPath = "Scenes/Javi/";
+
+	public void LoadLevel(string sceneName)
+	{
+		SceneManager.LoadScene (scenesPath + sceneName);
+	}
+}
