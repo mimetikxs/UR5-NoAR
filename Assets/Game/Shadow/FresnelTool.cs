8d8507563ae971cdd2a3eb3e1554a42f21d960f3
Nestor Rubio <nestor.kxs@gmail.com>
2018-03-06 13:38:13 +0100

    Working on desert scene

---
+++ Assets/Game/Shadow/FresnelTool.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Controls the rendering of the virtual tool.
+ * Updates position of the virtual tool.
+ * Holds state of the virtual tool.
+ */
+
+public class FresnelTool : MonoBehaviour {
+
+	public Transform tcp;
+
+	private Transform fresnel;
+	private Light spotLight;
+	private bool isOn;
+
+
+	private void Awake()
+	{
+		fresnel = this.transform.Find ("Fresnel");
+		spotLight = this.transform.Find ("Spotlight").GetComponent<Light> ();
+	}
+
+
+	void Start() 
+	{
+		isOn = false;
+	}
+	
+
+	void Update() 
+	{
+		this.transform.position = tcp.position;
+		this.transform.rotation = tcp.rotation;
+	}
+
+
+	private void OnDisable() 
+	{
+		SwitchOff ();
+	}
+
+
+	public void SwitchOn()
+	{
+		isOn = true;
+		spotLight.enabled = true;
+	}
+
+
+	public void SwitchOff() 
+	{
+		isOn = false;
+		spotLight.enabled = false;
+	}
+}
