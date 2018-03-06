e888857122867ae5b72e0650ef3cdf9d0ebbb040
Nestor Rubio <nestor.kxs@gmail.com>
2018-03-06 19:14:15 +0100

    Adding basic functionality to AR planet scene

---
+++ Assets/Game/Planet/MagnetTool.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Controls the rendering of the virtual tool.
+ * Updates position of the virtual tool.
+ * Holds state of the virtual tool.
+ */
+
+public class MagnetTool : MonoBehaviour {
+
+	public Transform tcp;
+
+	private Transform fresnel;
+	private bool isOn;
+
+
+	private void Awake()
+	{
+		fresnel = this.transform.Find ("Fresnel");
+	}
+
+
+	void Start() 
+	{
+		isOn = false;
+	}
+	
+
+	void Update() 
+	{
+		this.transform.position = tcp.position;
+		this.transform.rotation = tcp.rotation;
+	}
+
+
+	private void OnDisable() 
+	{
+		SwitchOff ();
+	}
+
+
+	public void SwitchOn()
+	{
+		isOn = true;
+		// TODO enable fx
+	}
+
+
+	public void SwitchOff() 
+	{
+		isOn = false;
+		// TODO disable fx
+	}
+}
---
+++ Assets/Game/Planet/RigMagnet.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Handles logic to extinguish fires.
+ * Controls the state of the virtual tool.
+ * 
+ * TODO: control position of hotspots
+ */
+
+public class RigMagnet : MonoBehaviour 
+{
+	//private Collider worldSphere;
+	private Transform toolTarget;
+	private MagnetTool magnetTool;
+
+	private bool isOn;
+
+
+	private void Awake()
+	{
+		magnetTool = transform.Find ("Tool").GetComponent<MagnetTool> ();
+		toolTarget = transform.Find ("ToolTarget");
+		//worldSphere = transform.Find ("WorldSphere").GetComponent<Collider> ();
+	}
+
+
+	private void Start() 
+	{
+		SwitchOff ();
+	}
+
+
+	private void Update() 
+	{
+		// update sphere scale
+		//worldSphere.transform.localScale = new Vector3 (sphereScale, sphereScale, sphereScale);
+
+		if (isOn) {
+			// detect collision of water stream with a fire cluster
+			// if colliding, begin substracting damage from tree
+		} 
+	}
+
+
+	public void SetToolTransform(Vector3 globalPosition, Quaternion globalRotation) 
+	{
+		toolTarget.position = globalPosition;
+		toolTarget.rotation = globalRotation;
+	}
+
+
+	public Transform GetToolTransform()
+	{
+		return toolTarget.transform;
+	}
+
+
+	public void SwitchOn() 
+	{
+		isOn = true;
+
+		magnetTool.SwitchOn ();
+	}
+
+
+	public void SwitchOff() 
+	{
+		isOn = false;
+
+		magnetTool.SwitchOff ();
+	}
+}
