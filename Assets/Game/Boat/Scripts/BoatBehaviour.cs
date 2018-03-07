7eb2ccb23f4f78f83cc61999d46d2f5eb937d2a1
Nestor Rubio <nestor.kxs@gmail.com>
2018-03-07 14:36:28 +0100

    Handling out of boat world

--- Assets/Game/Boat/Scripts/BoatBehaviour.cs
+++ Assets/Game/Boat/Scripts/BoatBehaviour.cs
@@ -14,6 +14,8 @@ public class BoatBehaviour : MonoBehaviour
 	private AudioSource collectedSound;
 	private ParticleSystem waterTrail;
 	private bool isOutsideWorld;
+	private Vector3 initialPos;
+	private Quaternion initialRot;
 
 	// delegated:
 	// triggered when waste is collected
@@ -31,13 +33,19 @@ public class BoatBehaviour : MonoBehaviour
 
 		collectedSound = GetComponent<AudioSource>();
 
-//		waterTrail = 
+		waterTrail = this.transform.Find ("Foam_FX").GetComponent<ParticleSystem> ();
+
+		initialPos = this.transform.position;
+		initialRot = this.transform.rotation;
 	}
 
 
 	private void FixedUpdate() 
 	{
 		rb.AddForceAtPosition (force * windScale, forcePoint.position, ForceMode.Acceleration);
+
+		if (isOutsideWorld)
+			rb.AddForceAtPosition(Vector3.down * 10f, forcePoint.position, ForceMode.Acceleration);
 
 		force = Vector3.zero; 
 	}
@@ -60,15 +68,38 @@ public class BoatBehaviour : MonoBehaviour
 
 	private void OnTriggerExit(Collider other)
 	{
-		if (other.name == "InWorldArea") 
+		// detect when the ship leaves the world
+		if (other.name == "InWorldArea" && isOutsideWorld == false) 
 		{
 			isOutsideWorld = true;
+
+//			waterTrail.gameObject.SetActive(false);
+
+			rb.constraints = RigidbodyConstraints.None;
+
+			Invoke("ResetToHome", 2f);
 		}
 	}
 
 
+	private void ResetToHome()
+	{
+		rb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+		rb.velocity = Vector3.zero;
+		rb.angularVelocity = Vector3.zero;
+
+		// put ship in initial position
+		this.transform.position = initialPos;
+		this.transform.rotation = initialRot;
+
+//		waterTrail.gameObject.SetActive(true);
+
+		isOutsideWorld = false;
+	}
+
+
 	// Normalized force (we only need direction)
-	public void addForce (Vector3 f) 
+	public void addForce(Vector3 f) 
 	{
 		force += f;
 	}
