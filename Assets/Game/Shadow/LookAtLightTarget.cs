a9fa69eb1f5d8dd420f1811f663c4aebbfe46847
Nestor Rubio <nestor.kxs@gmail.com>
2018-03-15 17:31:07 +0100

    Hotspots arranged in shadow scene

---
+++ Assets/Game/Shadow/LookAtLightTarget.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class LookAtLightTarget : MonoBehaviour 
+{
+	private Transform target;
+
+
+	void Awake()
+	{
+		target = transform.parent.Find ("LightTarget");	
+	}
+
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		//transform.rotation = Quaternion.FromToRotation (transform.position, target.position);
+		transform.LookAt(target.position);
+
+		transform.Rotate (90f, 0f, 0f);
+	}
+}
