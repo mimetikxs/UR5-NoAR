528f84e0b4c020f5fcccc95917a083de63dfbf2a
Nestor Rubio <nestor.kxs@gmail.com>
2018-03-16 18:16:47 +0100

    working on marble machine

---
+++ Assets/Game/MarbleMachine/MachineController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineController : MonoBehaviour 
+{
+	public Vector3 gravity = new Vector3();
+
+
+	void Awake()
+	{
+		Physics.gravity = gravity;
+	}
+
+
+	void Start() 
+	{
+	}
+
+
+	void Update() 
+	{
+	}
+
+
+	void FixedUpdate()
+	{
+	}
+}
---
+++ Assets/Game/MarbleMachine/RingRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingRotation : MonoBehaviour 
+{	
+	public float speedInner = 0f;
+	public float speedMiddle = 0f;
+	public float speedOuter = 0f;
+
+	private Transform inner;
+	private Transform middle;
+	private Transform outer;
+
+
+	void Awake()
+	{
+		inner = transform.Find ("Rings/Inner");
+		middle = transform.Find ("Rings/Middle");
+		outer = transform.Find ("Rings/Outer");
+	}
+
+
+	void Start() 
+	{
+	}
+	
+
+	void Update() 
+	{
+	}
+
+
+	void FixedUpdate()
+	{
+		inner.Rotate (0f, 0f, speedInner);
+		middle.Rotate (0f, 0f, speedMiddle);
+		outer.Rotate (0f, 0f, speedOuter);
+	}
+}
